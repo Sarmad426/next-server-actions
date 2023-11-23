519a57636467cde5129d30c5a389951aac0eb39a
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-23 21:04:08 +0500

    replaced form with component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { DeleteForm } from "@/components/delete-form";
-import { createTodo } from "@/lib/actions";
+import TodoForm from "@/components/todo-form";
 import prisma from "@/lib/prismadb";
 import { Todo } from "@prisma/client";
 
@@ -11,20 +11,7 @@ export default async function Home() {
   const todos: Todo[] = await getTodo();
   return (
     <main className="flex my-24 flex-col items-center justify-between p-24 text-white">
-      <form
-        action={createTodo}
-        className="flex items-center justify-center gap-x-5"
-      >
-        <input
-          type="text"
-          className="text-black w-[18rem] h-[2.3rem]"
-          name="title"
-          required
-        />
-        <button className="bg-white text-black p-3 rounded-md hover:bg-gray-100">
-          Submit
-        </button>
-      </form>
+      <TodoForm />
       <h2 className="text-2xl font-bold my-5">Todos</h2>
       <div className="flex flex-col items-center justify-normal gap-4 my-12">
         {todos.map((todo) => {
